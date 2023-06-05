d97d416a7b1c898d42c870f8afaeb7197e5b41ea
pradeep roy <pradeep.roy@gmail.com>
2023-06-05 12:01:52 +0530

    Write systemd unit file

--- build.sh
+++ build.sh
@@ -11,6 +11,12 @@ INSTALL_PREFIX=opt/zapier/pag
 PROG_DIR=${INSTALL_DIR}/${INSTALL_PREFIX}
 RELEASE_DIR=${PWD}/${OUTPUT_DIR}/release
 
+install_systemd_service() {
+    local system_dir="${INSTALL_DIR}/etc/systemd/system"
+    mkdir -p $system_dir
+    cp pag.service $system_dir
+}
+
 main() {
     local prog=${PROG_DIR}/prom-aggregation-gateway
     local config=$PWD/spec/pag.yml
@@ -18,6 +24,7 @@ main() {
     rm -r -f ${OUTPUT_DIR}
     mkdir -p ${PROG_DIR}
     go build -o $prog
+    install_systemd_service
 
     (
         mkdir ${RELEASE_DIR}
